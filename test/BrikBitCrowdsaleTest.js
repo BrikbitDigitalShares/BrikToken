fca18904401517a08938e77d09d08dda20aebd7c
Sfly <rocchitellifelice@gmail.com>
2018-04-03 18:33:58 +0200

    First & Second crowdsale integration

--- test/BrikBitCrowdsaleTest.js
+++ test/BrikBitCrowdsaleTest.js
@@ -1,17 +1,17 @@
 const BigNumber = web3.BigNumber;
 const chai = require('chai')
-  .use(require('chai-as-promised'))
-  .use(require('chai-bignumber')(BigNumber))
-  .should();
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
 
-const { expect } = chai
+const {expect} = chai
 const _ = require('lodash')
 
 const Brik = artifacts.require('BrikToken')
 const FirstCrowdsale = artifacts.require('FirstCrowdsale')
 const SecondCrowdsale = artifacts.require('SecondCrowdsale')
 
-const { ecsign } = require('ethereumjs-util')
+const {ecsign} = require('ethereumjs-util')
 const abi = require('ethereumjs-abi')
 const BN = require('bn.js')
 
@@ -24,104 +24,270 @@ const MAX_AMOUNT = '1000000000000000000'
 const errorMsg = 'VM Exception while processing transaction: revert';
 
 const getKycData = (userAddr, userid, icoAddr, pk) => {
-  // sha256("Eidoo icoengine authorization", icoAddress, buyerAddress, buyerId, maxAmount);
-  const hash = abi.soliditySHA256(
-    ['string', 'address', 'address', 'uint64', 'uint'],
-    ['Eidoo icoengine authorization', icoAddr, userAddr, new BN(userid), new BN(MAX_AMOUNT)]
-  )
-  const sig = ecsign(hash, pk)
-  return {
-    id: userid,
-    max: MAX_AMOUNT,
-    v: sig.v,
-    r: '0x' + sig.r.toString('hex'),
-    s: '0x' + sig.s.toString('hex')
-  }
-}
-
-const expectEvent = (res, eventName) => {
-  const ev = _.find(res.logs, { event: eventName })
-  expect(ev).to.not.be.undefined
-  return ev
+    // sha256("Eidoo icoengine authorization", icoAddress, buyerAddress, buyerId, maxAmount);
+    const hash = abi.soliditySHA256(
+        ['string', 'address', 'address', 'uint64', 'uint'],
+        ['Eidoo icoengine authorization', icoAddr, userAddr, new BN(userid), new BN(MAX_AMOUNT)]
+    )
+    const sig = ecsign(hash, pk)
+    return {
+        id: userid,
+        max: MAX_AMOUNT,
+        v: sig.v,
+        r: '0x' + sig.r.toString('hex'),
+        s: '0x' + sig.s.toString('hex')
+    }
 }
 
 contract('BrikBit ICO', function (accounts) {
 
-  before(async () => {
+    before(async () => {
 
-    this.token = await Brik.new()
-    this.owner = await token.owner()
-    this.firstCrowdsale = await FirstCrowdsale.new([SIGNER_ADDR], accounts[5], token.address, owner)
-    this.secondCrowdsale = await SecondCrowdsale.new([SIGNER_ADDR], accounts[6], token.address, owner)
+        this.token = await Brik.new()
+        this.owner = await token.owner()
+        this.firstCrowdsale = await FirstCrowdsale.new([SIGNER_ADDR], accounts[5], token.address, owner)
+        this.secondCrowdsale = await SecondCrowdsale.new([SIGNER_ADDR], accounts[6], token.address, owner)
 
-  })
-
-  describe('migrations deployment', async () => {
-
-    it('should deploy token contract correctly', async () => {
-      await Brik.new()
     })
 
-    it('should deploy firstCrowdsale contract correctly', async () => {
-      await FirstCrowdsale.new([SIGNER_ADDR], accounts[5], token.address, owner)
+    describe('migrations deployment', async () => {
+
+        it('should deploy token contract correctly', async () => {
+            await Brik.new()
+        })
+
+        it('should deploy firstCrowdsale contract correctly', async () => {
+            await FirstCrowdsale.new([SIGNER_ADDR], accounts[5], token.address, owner)
+        })
+
+        it('should deploy secondCrowdsale contract correctly', async () => {
+            await SecondCrowdsale.new([SIGNER_ADDR], accounts[6], token.address, owner)
+        })
     })
 
-    it('should deploy secondCrowdsale contract correctly', async () => {
-      await SecondCrowdsale.new([SIGNER_ADDR], accounts[6], token.address, owner)
-    })
-  })
+    describe('crowdsale approvement', async () => {
 
-  describe('crowdsale approvement', async () => {
+        it('should approve firstCrowdsale to sell on owner s behalf', async () => {
 
-    it('should approve firstCrowdsale to sell on owner s behalf', async () => {
+            await token.approve(firstCrowdsale.address, '25000000')
+            const allow = await token.allowance.call(owner, firstCrowdsale.address)
+            allow.should.be.bignumber.equal('25000000')
+        })
 
-      await token.approve(firstCrowdsale.address, '25000000')
-      const allow = await token.allowance.call(owner, firstCrowdsale.address)
-      allow.should.be.bignumber.equal('25000000')
-    })
+        it('should approve secondCrowdsale to sell on owner s behalf', async () => {
 
-    it('should approve secondCrowdsale to sell on owner s behalf', async () => {
-
-      await token.approve(secondCrowdsale.address, '75000000')
-      const allow = await token.allowance.call(owner, secondCrowdsale.address)
-      allow.should.be.bignumber.equal('75000000')
-    })
-  })
-
-  describe('crowdsales deployment tests', async () => {
-
-    it('should verify firstCrowdsale parameters', async () => {
-
-      const started = await firstCrowdsale.started();
-      const ended = await firstCrowdsale.ended();
-      const price = await firstCrowdsale.price();
-      const totalTokens = await firstCrowdsale.totalTokens();
-      const wallet = await firstCrowdsale.wallet();
-      const tokenOwner = await firstCrowdsale.tokenOwner();
-
-      started.should.be.true;
-      ended.should.be.false;
-      price.should.be.bignumber.equal('5000000');
-      totalTokens.should.be.bignumber.equal('25000000')
-      wallet.should.be.equal(accounts[5]);
-      tokenOwner.should.be.equal(owner);
+            await token.approve(secondCrowdsale.address, '75000000')
+            const allow = await token.allowance.call(owner, secondCrowdsale.address)
+            allow.should.be.bignumber.equal('75000000')
+        })
     })
 
-    it('should verify secondCrowdsale parameters', async () => {
+    describe('crowdsales deployment tests', async () => {
 
-      const started = await secondCrowdsale.started();
-      const ended = await secondCrowdsale.ended();
-      const totalTokens = await secondCrowdsale.totalTokens();
-      const wallet = await secondCrowdsale.wallet();
-      const tokenOwner = await secondCrowdsale.tokenOwner();
-      const price = await secondCrowdsale.price();
+        it('should verify firstCrowdsale parameters', async () => {
 
-      started.should.be.true;
-      ended.should.be.false;
-      totalTokens.should.be.bignumber.equal('75000000')
-      wallet.should.be.equal(accounts[6]);
-      tokenOwner.should.be.equal(owner);
-      console.log(price)
+            const started = await firstCrowdsale.started();
+            const ended = await firstCrowdsale.ended();
+            const price = await firstCrowdsale.price();
+            const totalTokens = await firstCrowdsale.totalTokens();
+            const wallet = await firstCrowdsale.wallet();
+            const tokenOwner = await firstCrowdsale.tokenOwner();
+
+            started.should.be.true;
+            ended.should.be.false;
+            price.should.be.bignumber.equal('5000000');
+            totalTokens.should.be.bignumber.equal('25000000')
+            wallet.should.be.equal(accounts[5]);
+            tokenOwner.should.be.equal(owner);
+        })
+
+        it('should verify secondCrowdsale parameters', async () => {
+
+            const started = await secondCrowdsale.started();
+            const ended = await secondCrowdsale.ended();
+            const totalTokens = await secondCrowdsale.totalTokens();
+            const wallet = await secondCrowdsale.wallet();
+            const tokenOwner = await secondCrowdsale.tokenOwner();
+            const price = await secondCrowdsale.price();
+
+            started.should.be.true;
+            ended.should.be.false;
+            totalTokens.should.be.bignumber.equal('75000000')
+            wallet.should.be.equal(accounts[6]);
+            tokenOwner.should.be.equal(owner);
+            //console.log(price)
+        })
     })
-  })
 })
+
+contract('First crowdsale KYC tests', function (accounts) {
+
+    before(async () => {
+
+        token = await Brik.new()
+        owner = await token.owner()
+        firstCrowdsale = await FirstCrowdsale.new([SIGNER_ADDR], accounts[5], token.address, owner)
+
+    })
+
+    describe('initialization', () => {
+
+        it('should save signer', async () => {
+
+            const sig = await firstCrowdsale.isKycSigner.call(SIGNER_ADDR)
+            const othSig = await firstCrowdsale.isKycSigner.call(OTHER_ADDR)
+
+            sig.should.be.true
+            othSig.should.be.false
+
+        })
+
+        it('should fail the default callback', async () => {
+            await firstCrowdsale.sendTransaction({
+                from: accounts[4],
+                to: firstCrowdsale.address,
+                value: MIN_CONTR
+            }).should.be.rejectedWith(errorMsg)
+        })
+    })
+
+    describe('buyTokens()', () => {
+
+        const buyer = accounts[4]
+        const wallet = accounts[2]
+
+        beforeEach(async () => {
+
+            token = await Brik.new()
+            owner = await token.owner()
+            firstCrowdsale = await FirstCrowdsale.new([SIGNER_ADDR], wallet, token.address, owner)
+            await token.approve(this.firstCrowdsale.address, 25000000 * 10 ** 18)
+        })
+
+        it('should reject for invalid signer', async () => {
+
+            const d = getKycData(buyer, 1, this.firstCrowdsale.address, OTHER_PK)
+            await this.firstCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value: MAX_AMOUNT
+            }).should.be.rejectedWith(errorMsg)
+        })
+
+        it('should fulfill', async () => {
+
+            const allow = await token.allowance.call(owner, firstCrowdsale.address)
+            const d = getKycData(buyer, 1, this.firstCrowdsale.address, SIGNER_PK)
+
+            allow.should.be.bignumber.equal(FIRST_CAP)
+            await firstCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value: MAX_AMOUNT
+            }).should.be.fulfilled
+        })
+
+        it('should reject for too big amount', async () => {
+            const d = getKycData(buyer, 1, this.firstCrowdsale.address, SIGNER_PK)
+            const value = web3.toBigNumber(MAX_AMOUNT).add(1)
+            await firstCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value
+            }).should.be.rejectedWith(errorMsg)
+        })
+
+        it('should reject for too little amount', async () => {
+            const d = getKycData(buyer, 1, this.firstCrowdsale.address, SIGNER_PK)
+            const value = web3.toBigNumber(MAX_AMOUNT).sub(1)
+            await firstCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value
+            }).should.be.rejectedWith(errorMsg)
+        })
+    })
+})
+
+contract('Second crowdsale KYC tests', function (accounts) {
+
+    before(async () => {
+
+        token = await Brik.new()
+        owner = await token.owner()
+        secondCrowdsale = await SecondCrowdsale.new([SIGNER_ADDR], accounts[5], token.address, owner)
+
+    })
+
+    describe('initialization', () => {
+
+        it('should save signer', async () => {
+
+            const sig = await secondCrowdsale.isKycSigner.call(SIGNER_ADDR)
+            const othSig = await secondCrowdsale.isKycSigner.call(OTHER_ADDR)
+
+            sig.should.be.true
+            othSig.should.be.false
+
+        })
+
+        it('should fail the default callback', async () => {
+
+            await secondCrowdsale.sendTransaction({
+                from: accounts[4],
+                to: secondCrowdsale.address,
+                value: MIN_CONTR
+            }).should.be.rejectedWith(errorMsg)
+        })
+    })
+
+    describe('buyTokens()', () => {
+
+        const buyer = accounts[4]
+        const wallet = accounts[2]
+
+        beforeEach(async () => {
+
+            token = await Brik.new()
+            owner = await token.owner()
+            secondCrowdsale = await SecondCrowdsale.new([SIGNER_ADDR], wallet, token.address, owner)
+            await token.approve(this.secondCrowdsale.address, 75000000 * 10 ** 18)
+        })
+
+        it('should reject for invalid signer', async () => {
+
+            const d = getKycData(buyer, 1, this.secondCrowdsale.address, OTHER_PK)
+            await this.secondCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value: MAX_AMOUNT
+            }).should.be.rejectedWith(errorMsg)
+        })
+
+        it('should fulfill', async () => {
+
+            const allow = await token.allowance.call(owner, secondCrowdsale.address)
+            const d = getKycData(buyer, 1, this.secondCrowdsale.address, SIGNER_PK)
+
+            allow.should.be.bignumber.equal(SECOND_CAP)
+            await secondCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value: MAX_AMOUNT
+            }).should.be.fulfilled
+        })
+
+        it('should reject for too big amount', async () => {
+            const d = getKycData(buyer, 1, this.secondCrowdsale.address, SIGNER_PK)
+            const value = web3.toBigNumber(MAX_AMOUNT).add(1)
+            await secondCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value
+            }).should.be.rejectedWith(errorMsg)
+        })
+
+        it('should reject for too little amount', async () => {
+            const d = getKycData(buyer, 1, this.secondCrowdsale.address, SIGNER_PK)
+            const value = web3.toBigNumber(MAX_AMOUNT).sub(1)
+            await secondCrowdsale.buyTokens(d.id, d.max, d.v, d.r, d.s, {
+                from: buyer,
+                value
+            }).should.be.rejectedWith(errorMsg)
+        })
+    })
+})
+
